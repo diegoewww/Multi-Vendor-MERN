32fa578816050118641b2397cc3f4664ddfcdf39
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-10-31 17:25:47 -0500

    first commit

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const ErrorHandler = require('./utils/ErrorHandler')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload({ useTempFiles: true }))
+
+// config
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config({
+    path: '../backend/config/.env'
+  })
+}
+
+// its for errorHandling
+app.use(ErrorHandler)
+module.exports = app
